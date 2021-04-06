132ecf637fa8e974707cd8e066616a19cde28fa2
mislav1 <mislav15matijevic@gmail.com>
2021-04-06 13:33:58 +0200

    added initial data api

--- server/routes/userRoutes/index.js
+++ server/routes/userRoutes/index.js
@@ -2,7 +2,9 @@ let express = require('express');
 let router = express.Router();
 
 let userAuthentication = require('./userAuthentication');
+let initialData = require("./initialData")
 
 router.use('/authentication', userAuthentication);
+router.use('/initial-data', initialData);
 
 module.exports = router;
---
+++ server/routes/userRoutes/initialData.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const router = express.Router()
+const db = require("../../config/database")
+const { httpStatus } = require('../../lib/constants')
+const auth = require("../../lib/userAuth")
+const {
+    getBadRequestResponse,
+    getSuccessResponse,
+    getInternalServerErrorResponse,
+    getNotFoundErrorResponse,
+} = require("../../lib/utils")
+
+router.get('/', auth(), async (req, res) => {
+    try {
+
+        const { identifier } = req.query;
+
+        if (!identifier) {
+            return res.send(getBadRequestResponse("Wrong parameters!"))
+        }
+
+        const queryCityData = `
+            SELECT id, name, identifier FROM City as c
+            WHERE c.identifier = ?
+        `
+        const [cities] = await db.query(queryCityData, {
+            replacements: [
+                identifier
+            ]
+        });
+
+        if (cities.length !== 1) {
+            return res.send(getNotFoundErrorResponse("City not found!"))
+        }
+
+        const initialData = {
+            city: cities[0],
+            cityRoutes: []
+        }
+
+        const queryCityRoutes = `
+            SELECT r.id, r.name, r.description, r.picturePath, r.cityId FROM Route as r
+            RIGHT JOIN City as c on c.id = r.cityId
+            WHERE c.identifier = ?
+        `
+        const [cityRoutes] = await db.query(queryCityRoutes, {
+            replacements: [
+                identifier
+            ]
+        });
+
+        for (let i = 0; i < cityRoutes.length; i++) {
+            const route = cityRoutes[i]
+            const queryRouteDestinations = `
+                SELECT d.id, d.name, d.description, d.picturePath, d.routeId, d.coordinates, ud.id as userVisited FROM Destination as d
+                LEFT JOIN UserDestination as ud on ud.destinationId = d.id and ud.userId = ?
+                WHERE routeId = ?
+            `
+            let [routeDestinations] = await db.query(queryRouteDestinations, {
+                replacements: [
+                    req.user.id,
+                    route.id
+                ]
+            });
+
+            routeDestinations = routeDestinations.map(d => ({
+                ...d,
+                userVisited: !!d.userVisited
+            }))
+
+            initialData.cityRoutes.push({
+                ...route,
+                routeDestinations
+            })
+        }
+
+        res.send(getSuccessResponse(initialData))
+    } catch (error) {
+        console.error(error)
+        return res.status(httpStatus.InternalServerError).send(getInternalServerErrorResponse(error.name || error.message))
+    }
+})
+
+
+module.exports = router;
