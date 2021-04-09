9e75716b9fa8e0c7b8348959a0a6ca48decbe988
mislav1 <mislav15matijevic@gmail.com>
2021-04-09 16:03:58 +0200

    add cors

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ let adminRoutes = require('./routes/adminRoutes');
 let userRoutes = require('./routes/userRoutes');
 const db = require("./config/database")
 const { httpStatus } = require("./lib/constants")
+const cors = require("cors")
 
 db.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
@@ -15,6 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(methodOverride('_method'));
+app.use(cors())
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(express.static(path.resolve(__dirname, '../uploads')));
@@ -23,16 +25,6 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
 
 app.get('/uploads/*', (req, res) => {
-  /*if (fs.existsSync(path.resolve(__dirname, '../uploads', req.url.split('/').pop()))) {
-    res.sendFile(path.resolve(__dirname, '../uploads', req.url.split('/').pop()));
-  } 
-  else {
-    res.send({
-      status: httpStatus.NotFound,
-      error: 'Image not found',
-      data: null
-    });
-  }*/
   res.sendFile(path.resolve(__dirname, '../uploads', req.url.split('/').pop()));
 });
 
