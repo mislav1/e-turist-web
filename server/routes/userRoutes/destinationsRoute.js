1a9f76a472748582d37f66f0b345acaa428c05b3
mislav1 <mislav15matijevic@gmail.com>
2021-06-01 19:47:30 +0200

    add average and my rating to visited destination api

--- server/routes/userRoutes/destinationsRoute.js
+++ server/routes/userRoutes/destinationsRoute.js
@@ -34,7 +34,7 @@ router.post('/add-visited', auth(), async (req, res) => {
             ]
         });
 
-        if(visited.length > 0){
+        if (visited.length > 0) {
             return res.send(getBadRequestResponse("Destinacija je već posjećena!"))
         }
 
@@ -59,30 +59,54 @@ router.get('/visited-by-user', auth(), async (req, res) => {
     try {
 
         let userId = req.user.id
+
         let { limit, page } = req.query;
 
-        if(!limit || limit < 1) limit = 5;
+        if (!limit || limit < 1) limit = 5;
         else limit = parseInt(limit)
 
-        if(!page || page < 1) page = 1;
+        if (!page || page < 1) page = 1;
         else page = parseInt(page)
 
         const queryVisitedByUser = `
-            SELECT ud.modifiedAt as visitedAt, d.* FROM  UserDestination as ud
+            SELECT ud.modifiedAt as visitedAt, d.*, r.rating as myRating FROM UserDestination as ud
             LEFT JOIN Destination as d on ud.destinationId = d.id
-            WHERE ud.userId = ?
+            LEFT JOIN Rating as r on ud.destinationId = r.destinationId
+            WHERE ud.userId = ? and d.isDeleted = false and (r.userId = ? or r.userId is null)
             ORDER BY ud.modifiedAt desc
             LIMIT ?, ? 
         `
-        const [visitedDestinations] = await db.query(queryVisitedByUser, {
+        let [visitedDestinations] = await db.query(queryVisitedByUser, {
             replacements: [
+                userId,
                 userId,
                 (page - 1) * limit,
                 limit
             ]
         });
 
-        res.send(getSuccessResponse({visitedDestinations}))
+
+        // GET AVERAGE RATINGS FOR DESTINATIONS
+        const queryAverageRatingsForDestinations = `
+            SELECT r.destinationId, AVG(rating) AS 'averageRating'
+            FROM Rating as r
+            where r.destinationId is not null
+            GROUP BY r.destinationId
+        `
+        const [averageRatingsForDestinations] = await db.query(queryAverageRatingsForDestinations);
+
+        let averageDestObj = {}
+
+        averageRatingsForDestinations.forEach(d => averageDestObj[d.destinationId] = d.averageRating)
+        
+        visitedDestinations = visitedDestinations.map((d) => {
+            return {
+                ...d,
+                averageRating: averageDestObj[d.id] ? Number(averageDestObj[d.id]) : null
+            }
+        })
+
+        res.send(getSuccessResponse({ visitedDestinations }))
     } catch (error) {
         console.error(error)
         return res.status(httpStatus.InternalServerError).send(getInternalServerErrorResponse(error.name || error.message))
